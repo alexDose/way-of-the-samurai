1f4f347c83819d0c8e8dd3feaff94800dec7f13f
Alexei <i0ver@mail.ru>
2022-07-07 21:07:30 +0300

    47 lesson

--- src/redux/profile-reducer.jsx
+++ src/redux/profile-reducer.jsx
@@ -28,21 +28,26 @@ let initialState =  {
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText
-            return state
-        case ADD_POST:
+            let stateCopy = {...state}
+            stateCopy.newPostText = action.newText
+            return stateCopy
+
+        case ADD_POST: {
             let newPost = {
                 id: 4,
                 message: state.newPostText,
                 like: 0,
                 src: "https://images.vexels.com/media/users/3/145908/raw/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"
             }
-            state.posts.push(newPost)
-            state.newPostText = ""
-            return state
+            let stateCopy = {...state}
+            stateCopy.posts = [...state.posts]
+            stateCopy.posts.push(newPost)
+            stateCopy.newPostText = ""
+            return stateCopy
+        }
         default:
             return state
-    }
+        }
 }
 
 export const addPostActionCreator = () => ({type: ADD_POST})
