3f484db937d711707e767942c15404053a99a3e5
Alexei <i0ver@mail.ru>
2022-07-07 21:49:51 +0300

    48 lesson

--- src/redux/dialogs-reducer.jsx
+++ src/redux/dialogs-reducer.jsx
@@ -21,12 +21,9 @@ const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case SEND_MESSAGE:
             let body = state.newMessageBody
-            state.messages.push({id: 4, message: body})
-            state.newMessageBody = ""
-            return state
+            return  {...state, newMessageBody: "", messages: [...state.messages, {id: 4, message: body}]}
         case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body
-            return state
+            return  {...state, newMessageBody: action.body}
         default:
             return state
     }
--- src/redux/profile-reducer.jsx
+++ src/redux/profile-reducer.jsx
@@ -28,9 +28,7 @@ let initialState =  {
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_POST_TEXT:
-            let stateCopy = {...state}
-            stateCopy.newPostText = action.newText
-            return stateCopy
+            return  {...state, newPostText: action.newText}
 
         case ADD_POST: {
             let newPost = {
@@ -39,11 +37,7 @@ const profileReducer = (state = initialState, action) => {
                 like: 0,
                 src: "https://images.vexels.com/media/users/3/145908/raw/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"
             }
-            let stateCopy = {...state}
-            stateCopy.posts = [...state.posts]
-            stateCopy.posts.push(newPost)
-            stateCopy.newPostText = ""
-            return stateCopy
+            return  {...state, posts: [...state.posts, newPost], newPostText: ""}
         }
         default:
             return state
