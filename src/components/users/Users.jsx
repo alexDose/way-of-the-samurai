fe5c302e01e09bbba6cec2980c3f4687f64761db
Alexei <i0ver@mail.ru>
2022-07-08 14:33:27 +0300

    51 lesson

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,56 +1,43 @@
 import React from "react";
 import s from "./Users.module.css"
+import userPhoto from "../assets/images/user.png"
+import * as axios from "axios";
 
 export const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([{
-            id: 1,
-            photoUrl: "https://play-lh.googleusercontent.com/CWzqShf8hi-AhV9dUjzsqk2URzdIv8Vk2LmxBzf-Hc8T-oGkLVXe6pMpcXv36ofpvtc",
-            followed: false,
-            fullName: "Ivan",
-            status: "I am a boss",
-            location: {city: "Minsk", country: "Belarus"}
-        },
-            {
-                id: 2,
-                photoUrl: "https://play-lh.googleusercontent.com/CWzqShf8hi-AhV9dUjzsqk2URzdIv8Vk2LmxBzf-Hc8T-oGkLVXe6pMpcXv36ofpvtc",
-                followed: true,
-                fullName: "Alex",
-                status: "I am a boss too",
-                location: {city: "Kiev", country: "Ukraine"}
-            },
-            {
-                id: 3,
-                photoUrl: "https://play-lh.googleusercontent.com/CWzqShf8hi-AhV9dUjzsqk2URzdIv8Vk2LmxBzf-Hc8T-oGkLVXe6pMpcXv36ofpvtc",
-                followed: false,
-                fullName: "Sveta",
-                status: "I am a boss too",
-                location: {city: "Moscow", country: "Russia"}
-            }])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            debugger
+            props.setUsers(response.data.items)
+        })
     }
+
     return (
         <div>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img className={s.userPhoto} src={u.photoUrl}/>
+                            <img className={s.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto}/>
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                                : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
+                                ? <button onClick={() => {
+                                    props.unfollow(u.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    props.follow(u.id)
+                                }}>Follow</button>}
                         </div>
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
                 </div>)
