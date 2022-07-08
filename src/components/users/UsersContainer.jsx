0d5b2475e5790d275defec9a9d890141766759ea
Alexei <i0ver@mail.ru>
2022-07-08 17:47:02 +0300

    55 lesson

--- src/components/users/UsersContainer.jsx
+++ src/components/users/UsersContainer.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import {connect} from "react-redux";
-import {followActionCreator, setUsersActionCreator, unfollowActionCreator} from "../../redux/users-reducer";
+import {
+    followActionCreator,
+    setCurrentPageActionCreator,
+    setUsersActionCreator, setUsersTotalCountActionCreator,
+    unfollowActionCreator
+} from "../../redux/users-reducer";
 import {Users} from "./Users";
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
@@ -19,6 +27,12 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersActionCreator(users))
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageActionCreator(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setUsersTotalCountActionCreator(totalCount))
         }
     }
 }
