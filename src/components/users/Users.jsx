4307d9f1d9d52a8f54dafe5438a2090a138e338f
Alexei <i0ver@mail.ru>
2022-07-09 22:58:51 +0300

    62 lesson follow/unfollow

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./Users.module.css";
 import userPhoto from "../assets/images/user.png";
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 export const Users = (props) => {
 
@@ -32,10 +33,22 @@ export const Users = (props) => {
                         <div>
                             {u.followed
                                 ? <button onClick={() => {
-                                    props.unfollow(u.id)
+                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {withCredentials: true, headers: {
+                                        "API-KEY": "e313faef-3b0b-4b33-b1e4-6d13acc82983"
+                                        }}).then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.unfollow(u.id)
+                                        }
+                                    })
                                 }}>Unfollow</button>
                                 : <button onClick={() => {
-                                    props.follow(u.id)
+                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {withCredentials: true, headers: {
+                                            "API-KEY": "e313faef-3b0b-4b33-b1e4-6d13acc82983"
+                                        }}).then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.follow(u.id)
+                                        }
+                                    })
                                 }}>Follow</button>}
                         </div>
                     </span>
--- src/components/users/UsersContainer.jsx
+++ src/components/users/UsersContainer.jsx
@@ -18,7 +18,7 @@ class UsersContainer extends React.Component {
     */
     componentDidMount() {
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {withCredentials: true}).then(response => {
             this.props.toggleIsFetching(false)
             this.props.setUsers(response.data.items)
             this.props.setTotalUserCount(response.data.totalCount)
@@ -28,7 +28,7 @@ class UsersContainer extends React.Component {
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber)
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {withCredentials: true}).then(response => {
             this.props.toggleIsFetching(false)
             this.props.setUsers(response.data.items)
         })
