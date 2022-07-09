4425442821dbb3b78942a7feb5a2fe9a57e207c4
Alexei <i0ver@mail.ru>
2022-07-09 15:57:24 +0300

    61 lesson login, auth/me

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css"
-import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import News from "./components/news/News";
 import Music from "./components/music/Music";
@@ -9,25 +8,25 @@ import {Route, Routes} from "react-router-dom";
 import {DialogsContainer} from "./components/dialogs/DialogsContainer";
 import UsersContainer from "./components/users/UsersContainer";
 import ProfileContainer from "./components/profile/ProfileContainer";
+import HeaderContainer from "./components/HeaderContainer";
 
 const App = () => {
 
     return (
-            <div className="app-wrapper">
-                <Header/>
-                <Navbar/>
-                <div className="app-wrapper-content">
-                    <Routes>
-                        <Route path="/dialogs"
-                               element={<DialogsContainer/>}/>
-                        <Route path="/profile/:userId" element={<ProfileContainer/>}/>
-                        <Route path="/users" element={<UsersContainer/>}/>
-                        <Route path="/news/*" element={<News/>}/>
-                        <Route path="/music/*" element={<Music/>}/>
-                        <Route path="/settings/*" element={<Settings/>}/>
-                    </Routes>
-                </div>
+        <div className="app-wrapper">
+            <HeaderContainer/>
+            <Navbar/>
+            <div className="app-wrapper-content">
+                <Routes>
+                    <Route path="/dialogs" element={<DialogsContainer/>}/>
+                    <Route path="/profile/:userId" element={<ProfileContainer/>}/>
+                    <Route path="/users" element={<UsersContainer/>}/>
+                    <Route path="/news/*" element={<News/>}/>
+                    <Route path="/music/*" element={<Music/>}/>
+                    <Route path="/settings/*" element={<Settings/>}/>
+                </Routes>
             </div>
+        </div>
     );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import s from "./Header.module.css"
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img src="https://i.pinimg.com/736x/e9/e2/78/e9e2787d0cb55d570fe1c76843506759.jpg"/>
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink> }
+            </div>
         </header>
     );
 }
