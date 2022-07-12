e3ef1104e5ada98f48d0fb88859da9d30bc6e264
Alexei <i0ver@mail.ru>
2022-07-12 16:31:23 +0300

    75 lesson redux-form(don't work)

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
 
-export const Login = () => {
-    return <h2>LOGIN</h2>
+const LoginForm = (props) => {
+    return <div>
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field name={"Login"} placeholder={"Login"} component={"input"}/>
+            </div>
+            <div>
+                <Field name={"Password"} placeholder={"Password"} component={"input"}/>
+            </div>
+            <div>
+                <Field name={"rememberMe"} type="checkbox" component={"input"}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    </div>
 }
+
+const LoginReduxForm = reduxForm({
+    form: "login"
+})(LoginForm)
+
+export const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+    return <div>
+        <h2>Login</h2>
+        <LoginReduxForm onSubmit={onSubmit()}/>
+    </div>
+}
+
