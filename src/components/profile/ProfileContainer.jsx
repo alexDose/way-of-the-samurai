8a4bae435b8cd95e5e3c83e9ed7342ac888355f0
Alexei <i0ver@mail.ru>
2022-07-13 14:55:27 +0300

    79 lesson stopSubmit

--- src/components/profile/ProfileContainer.jsx
+++ src/components/profile/ProfileContainer.jsx
@@ -20,7 +20,7 @@ class ProfileContainer extends React.Component {
 
         let userId = this.props.match.params.userId
         if (!userId) {
-            userId = 23450
+            userId = this.props.authorizedUserId  /*23450*/
         }
         this.props.getUserProfile(userId)
         this.props.getStatus(userId)
@@ -40,7 +40,9 @@ class ProfileContainer extends React.Component {
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
-    status: state.profilePage.status
+    status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth
 })
 
 export default compose(
